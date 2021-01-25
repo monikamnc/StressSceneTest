ade0ef1167a29ff5bf57a8207f3a82c68fafa1b5
Monica <monicamartin13@hotmail.com>
2021-01-25 18:11:27 +0100

    Función hago cosis

--- Assets/Scripts/BotonCosis.cs
+++ Assets/Scripts/BotonCosis.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class BotonCosis : MonoBehaviour
 {
+	GameObject Sphere;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Sphere = GameObject.Find("GameController").GetComponent<GameController>();
     }
 
     // Update is called once per frame
@@ -15,4 +16,7 @@ public class BotonCosis : MonoBehaviour
     {
         
     }
+public void HagoCosis(){
+	Sphere.transform.position = New Vector3( transform.position.x + 2,transform.position.y,transform.position.z);
+}
 }
